d7e536bc4d0935e4f1f958beeb5f788019d02384
ling <64818614@qq.com>
2024-07-06 16:48:05 +0800

    structure

--- packages/index.ts
+++ packages/index.ts
@@ -1,7 +1,9 @@
 // import { Button } from './stories/Button';
 // export type { ButtonProps } from './stories/Button';
 
-export { Button } from './stories/Button';
-export type { ButtonProps } from './stories/Button';
+// export { annotation, default } from './annotation/annotation';
+// export type { ButtonProps } from './stories/Button';
+
+export { Annotation } from './annotation/Annotation';
 
 // import './styles/index.less';
