1f80d3e4432705405156214d16119d20ecf61969
ling <64818614@qq.com>
2024-07-11 10:52:09 +0800

    remove console

--- vite.config.ts
+++ vite.config.ts
@@ -21,6 +21,9 @@ export default defineConfig({
       },
     },
   },
+  esbuild: {
+    drop: ['console', 'debugger'],
+  },
   resolve: {
     alias: {
       '@': resolve(__dirname, './packages'),
